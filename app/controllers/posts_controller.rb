30d01f3145c1951e21098839595805037fd6de80
chantaimen <chantaimen955@gmail.com>
2017-04-22 13:30:53 +0000

    First commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,62 @@
+class PostsController < ApplicationController 
+    before_action :authenticate_user!  
+    before_action :set_post, only: [:show, :edit, :update, :destroy]
+    before_action :owned_post, only: [:edit, :update, :destroy]
+
+    def index
+        @posts = Post.all
+    end
+
+    def show
+    end
+
+    def new
+        @post = current_user.posts.build
+    end
+
+    def create
+        @post = current_user.posts.build(post_params)
+        if @post.save
+            flash[:success] = "Your post has been created!"
+            redirect_to posts_path
+        else
+            flash.now[:alert] = "ERROR. PLEASE CHECK INPUT"
+            render :new
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if @post.update(post_params)
+            flash[:success] = "Post updated."
+            redirect_to posts_path
+        else
+            flash.now[:alert] = "Failed.  PLEASE CHECK INPUT"
+            render :edit
+        end
+    end
+
+    def destroy
+        @post.destroy
+        redirect_to root_path
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:image, :caption)
+    end
+
+    def set_post
+        @post = Post.find(params[:id])
+    end
+    
+    def owned_post  
+        unless current_user == @post.user
+            flash[:alert] = "That post doesn't belong to you!"
+            redirect_to root_path
+        end
+    end  
+end 
